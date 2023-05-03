b79f210b3519630489252d24089ad1fee17518b6
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-05-03 20:47:51 +0600

    gitignore-file-create

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express= require('express')
 const app =express();
+const cors = require('cors');
 const port = 5000;
+app.use(cors())
+const data= require('./date/data.json')
 
 app.get('/',(req,res)=>{
     res.send('hello world')
+})
+app.get('/news',(req,res)=>{
+    res.send(data)
 })
 
 app.listen(port,()=>{
